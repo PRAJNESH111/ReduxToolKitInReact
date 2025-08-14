c307edd1e634cef049e2e9ff7f5bfc5bc4f9ef67
PRAJNESH111 <prajneshpajju094@gmail.com>
2025-08-14 12:44:10 +0530

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -46,7 +46,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter basename="/ReduxToolKitInReact">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
